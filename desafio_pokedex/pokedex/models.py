8eb224b6919e020aea876c7f3552e4fc6d92b824
Olaeverton5 <pmelaeduarda2015@gmail.com>
2019-05-02 21:28:08 -0300

    Fazendo com que os anexos funcione

--- desafio_pokedex/pokedex/models.py
+++ desafio_pokedex/pokedex/models.py
@@ -17,12 +17,14 @@ class Habilidade(models.Model):
     nome = models.CharField(max_length=120)
     descricao = models.CharField(max_length=200,verbose_name="Descrição/Sobre")
 
+    def __str__(self):
+        return self.nome
 
 class Pokemon(models.Model):
     imagem = models.FileField(upload_to='fotos/')
     nome = models.CharField(max_length=120)
     altura = models.FloatField()
-    fk_categoria = models.ForeignKey('Categoria',on_delete=models.PROTECT)
+    fk_categoria = models.ForeignKey('Categoria', on_delete=models.PROTECT)
     fk_habilidades = models.ManyToManyField(Habilidade)
     ponto_saude = models.PositiveIntegerField()
     ataque = models.PositiveIntegerField()
@@ -31,3 +33,5 @@ class Pokemon(models.Model):
     defesa_especial = models.PositiveIntegerField()
     velocidade = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.nome
