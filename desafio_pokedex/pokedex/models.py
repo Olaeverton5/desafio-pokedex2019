8d83a2423b5d6dbfd7d2b578e071d6e632938755
Olaeverton5 <pmelaeduarda2015@gmail.com>
2019-05-02 21:10:28 -0300

    Criando funções para conversão de das models

--- desafio_pokedex/pokedex/models.py
+++ desafio_pokedex/pokedex/models.py
@@ -6,9 +6,17 @@ from django.db import models
 class Categoria(models.Model):
     nome = models.CharField(max_length=120, unique=True)
 
+    def __str__(self):
+        return self.nome
+
+    def __str__(self):
+        return self.id
+
+
 class Habilidade(models.Model):
     nome = models.CharField(max_length=120)
     descricao = models.CharField(max_length=200,verbose_name="Descrição/Sobre")
+
 
 class Pokemon(models.Model):
     imagem = models.FileField(upload_to='fotos/')
